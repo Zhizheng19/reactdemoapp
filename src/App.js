4fabaf4e74947bbb87f9e634103f230933eabc0e
Dong <Zdong3119@conestogac.on.ca>
2025-07-08 12:00:22 -0400

    Added login page. Included conditional error conditional error message for empty input.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './Home.js';
 import About from "./About.js";
+import LoginPage from "./pages/LoginPage.js";
 
 function App() {
   console.log("App called");
   return (
     <BrowserRouter>
       <nav>
-        <Link to='/'>Home</Link> | <Link to="/about">About</Link>
+       <Link to='/'>Home</Link> | <Link to="/about">About</Link> | <Link to='/login'> Log In</Link>
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path="/login" element={<LoginPage />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+function LoginPage() {
+    
+    console.log("LoginPage Called");
+    const [userName, setUserName] = useState("");
+    const [error, setError] = useState(false); // let error = false;  Don't do this and change it to true in the call back
+    const navigate = useNavigate();
+    
+    // call back functions
+    const saveUserName = (e) => {
+        setUserName(e.target.value);
+        setError(false);
+    };
+    const handleSubmit = () => {
+        if (!userName.trim()) {
+            setError(true);
+            // show alert: the user's name is required 
+        }
+        else {
+            navigate("/order", { state: { userName } });
+        }
+    };
+    return (
+        <>
+            <h2>Welcome to Domi Pizza Wagon</h2>
+            <label htmlFor="userNameInput">Name:</label>
+            {error && (
+                <div style={{ color: "red" }}>this field is required</div>
+            )}
+            <br />
+            <input id="userNameInput" type="text" placeholder="Enter your name" value={userName} onChange={saveUserName} />
+            <button onClick={handleSubmit}>Submit</button>
+        </>
+    );
+}
+export default LoginPage;
